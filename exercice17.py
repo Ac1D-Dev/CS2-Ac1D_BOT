50f74fdf4781f943659c08c94ddf170919de873b
Tom <tomtoussaint5@gmail.com>
2026-09-07 04:55:15 +0200

    debug

--- exercice17.py
+++ exercice17.py
@@ -14,7 +14,7 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-@bot.event()
+@bot.event
 async def on_ready():
     print(f"Connecté en tant que {bot.user}")
 
