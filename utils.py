cdb08710508bba495fe27449711a4943ae96ba37
Tom <tomtoussaint5@gmail.com>
2026-09-08 05:34:14 +0200

    Utils minor fix

--- utils.py
+++ utils.py
@@ -60,9 +60,9 @@ class ViewMode(discord.ui.View):
 
             embed.set_thumbnail(url=self.player_infos["avatar"])
             embed.add_field(name="Average K/D", value=self.player_infos["avgkd"], inline=True)
-            embed.add_field(name="Average Headshot %", value=self.player_infos["hsavg"], inline=True)
-            embed.add_field(name="\nADR", value=self.player_infos["adr"], inline=True)
             embed.add_field(name="Winrate", value=self.player_infos["winrate"], inline=True)
+            embed.add_field(name="Average Headshot %", value=self.player_infos["hsavg"], inline=True)
+            embed.add_field(name="ADR", value=self.player_infos["adr"], inline=True)
             
 
         elif select_preset == "Competitive":
