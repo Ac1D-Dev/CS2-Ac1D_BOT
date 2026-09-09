b8d7a0c36d03a6611b499a4271da8c4b722d9c7e
Tom <tomtoussaint5@gmail.com>
2026-09-09 04:54:52 +0200

    refactor: ameliore l'aspect des messages d'erreur

--- errors.py
+++ errors.py
@@ -6,29 +6,30 @@ async def handle_error_status(response, pseudo, interaction):
     if response.status_code == 401:
         embed = discord.Embed(title= "**- Error 401 : Unauthorized -**", color=color_per_level[1])
     
-        embed.add_field(name= f"401 :",value= " invalid Bot config", inline=True)
+        embed.add_field(name= f"401 :",value= " **invalid Bot config**", inline=True)
         await interaction.response.send_message(embed=embed)
         return True
             
     elif response.status_code == 404:
         embed = discord.Embed(title= "**- Error 404 : Not Found -**", color=color_per_level[1])
     
-        embed.add_field(name= f"**404 :{pseudo} -**",value= " invalid pseudo", inline=True)
+        embed.add_field(name= f"**404 :{pseudo} **",value= " **invalid pseudo**", inline=True)
         await interaction.response.send_message(embed=embed)
         return True
 
     elif response.status_code == 429:
         embed = discord.Embed(title= "**- Error 429 : Too Many Requests -**", color=color_per_level[1])
         
-        embed.add_field(name= f"**429 :**",value= " Too Many Requests", inline=True)
+        embed.add_field(name= f"**429 :**",value= " **Too Many Requests**", inline=True)
         await interaction.response.send_message(embed=embed)
         return True
 
-
+    #TODO inserer code de status dans le nom de l'erreur
     elif response.status_code >= 500:
         embed = discord.Embed(title= "**- Error 5xx (500-599) : FaceIt Server Failed -**", color=color_per_level[1])
         
-        embed.add_field(name= f"**5xx :**",value= " Server Failed\n (Server Error / Bad Gateway / Service Unavailable)", inline=True)
+        embed.add_field(name= f"**5xx :**",value= " **Server Failed**\n"
+                        "**(Server Error / Bad Gateway / Service Unavailable)**", inline=True)
         await interaction.response.send_message(embed=embed)
         return True
     
