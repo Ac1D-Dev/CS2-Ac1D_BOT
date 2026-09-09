50bde190a7a863d557f55ecc950068e8d5bb72fc
Tom <tomtoussaint5@gmail.com>
2026-09-10 01:24:36 +0200

    handler correction

--- cs2_bot.py
+++ cs2_bot.py
@@ -34,7 +34,7 @@ async def on_app_command_error(interaction, error):
         embed = discord.Embed(title= "",description=f"Active CoolDown\n"
                                                     f" Retry in {error.retry_after} secondes"
                                                     )
-        await interaction.response.send_message(embed=embed)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
 @bot.tree.command(name="ping", description="Detection Test for development")
 async def ping(interaction: discord.Interaction):
