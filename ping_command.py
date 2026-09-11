7d8f08dc9f3dfec6129543a9ad710f9ac359f536
Tom <tomtoussaint5@gmail.com>
2026-09-11 20:45:58 +0200

    /ping

--- ping_command.py
+++ ping_command.py
@@ -1,7 +1,17 @@
 import discord
+import datetime
+import faceit_api
 
 def setup(bot):
 
+    latency = bot.latency * 1000
+    ms = round(latency, 2)
+
     @bot.tree.command(name="ping", description="Detection Test for development")
     async def ping(interaction: discord.Interaction):
-        await interaction.response.send_message(f"Connection Established {bot.user} !")
+        embed = discord.Embed(title=f"- Ping Test -")
+        
+        embed.add_field(name=f"**Ping : {ms} ms\n**")
+        embed.add_field(name=f"**\nCS2 Bot Tracker is Connected**")
+        embed.timestamp = datetime.datetime.now()
+        await interaction.response.send_message(embed= embed)
