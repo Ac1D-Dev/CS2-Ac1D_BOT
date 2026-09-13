b78b48539c23beda8c785c3048c6c0de58816965
Tom <tomtoussaint5@gmail.com>
2026-09-14 01:13:18 +0200

    -embed fix

--- commands/setnick_command.py
+++ commands/setnick_command.py
@@ -18,6 +18,10 @@ def setup(bot, FACEIT_KEY):
         
         embed = discord.Embed(title= "🔗 Link FaceIt Account", description=f"Tu es sur le point de lier ton compte à {pseudo_faceit['pseudo']}, confirmer ?" , color= utils.color_per_level[pseudo_faceit['level']])
         embed.set_author(name=pseudo_faceit["pseudo"], icon_url=pseudo_faceit["avatar"])
+        embed.add_field(name=f"** Discord User Account **", value=f"** \n {interaction.user} **", inline=True)
+        embed.add_field(name="FaceIt Account",value=f"\n {pseudo_faceit['pseudo']} \n"
+                                                    f"\n🎖️ Level : {pseudo_faceit['level']} \n"
+                                                    f"\n📈 Elo : {pseudo_faceit['elo']} ",inline=True)
         
         profile_view = views.ViewProfile(pseudo_faceit= pseudo_faceit)
         await interaction.response.send_message(embed=embed, view=profile_view)
